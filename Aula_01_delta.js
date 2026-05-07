bf7eec76f29bb1cacff89e88179db844711c623d
rlincon <82413456+rlincon@users.noreply.github.com>
2026-05-07 12:10:18 -0300

    Add files via upload

---
+++ Aula_01_delta.js
@@ -0,0 +1,11 @@
+b= 6;
+b = b*-1;
+delta = 20;
+d = delta*delta/delta;
+
+a = 5;
+x=-b+d/2*a;
+c = 10;
+b = b*b-4*a*c;
+console.log('O valor de delta é : ', x);
+console.log('O valor de delta é : ', b);
