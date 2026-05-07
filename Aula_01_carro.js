01d1c064cf2b7214aafaf29b7fc639c9504f0e42
rlincon <82413456+rlincon@users.noreply.github.com>
2026-05-07 13:00:03 -0400

    Add files via upload

---
+++ Aula_01_carro.js
@@ -0,0 +1,13 @@
+carro1 = "fusca";
+carro2 = "chevette";
+
+kmcarro1 = 100;
+kmcarro2 = 360;
+
+precokm1 = 12.70;
+precokm2 = 21.70;
+
+totalcarro1 = kmcarro1*precokm1;
+totalcarro2 = kmcarro2*precokm2;
+
+console.log(totalcarro1,totalcarro2);
